c3f4b13d62180454657f3b3e7055d91cc70bbc09
JuanTLA16 <125713407+JuanTLA16@users.noreply.github.com>
2023-02-25 09:53:16 -0500

    Add files via upload

---
+++ Numero_3_digitos.py
@@ -0,0 +1,18 @@
+print("------------------------------")
+print("--------NUMERO 3 DIGITOS------")
+print("------------------------------")
+
+#input
+
+n = int(input("Digite el valor de n: "))
+
+## Processing
+
+n = 3*100+21
+
+print("Resultado: " + str(n))
+
+
+print("--------------------------------")
+print("---------RESULTADO--------------")
+print("--------------------------------")
